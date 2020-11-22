fb750f6761483cec1ac2cb753977b552ecc99162
Willberg <hszhongtao@163.com>
2020-11-23 00:09:31 +0800

    第5章

---
+++ src/ch5/findlinks1/findlinks.go
@@ -0,0 +1,33 @@
+// 解析html并找到links
+package main
+
+import (
+	"fmt"
+	html "golang.org/x/net/html"
+	"os"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
